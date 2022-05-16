c3fcab7535c8c00edf90d086119051d3b687d7d6
usbharu <64310155+usbharu@users.noreply.github.com>
2022-05-17 08:43:33 +0900

    add getAuthor

--- dev4j-core/src/main/java/io/github/usbharu/dev4j/core/AuthorProcessor.java
+++ dev4j-core/src/main/java/io/github/usbharu/dev4j/core/AuthorProcessor.java
@@ -1,18 +1,21 @@
 package io.github.usbharu.dev4j.core;
 
 import com.google.auto.service.AutoService;
-import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.MethodTree;
-import com.sun.source.util.TreePath;
-import com.sun.source.util.TreeScanner;
 import com.sun.source.util.Trees;
+import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.model.JavacElements;
 import com.sun.tools.javac.parser.ParserFactory;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
-import com.sun.tools.javac.tree.JCTree.JCMethodDecl;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.JCTree.JCBlock;
+import com.sun.tools.javac.tree.JCTree.JCClassDecl;
+import com.sun.tools.javac.tree.JCTree.JCReturn;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.ListBuffer;
+import com.sun.tools.javac.util.Names;
+import io.github.usbharu.dev4j.api.Author;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
@@ -32,46 +35,45 @@ public class AuthorProcessor extends AbstractProcessor {
   private Trees trees;
   private Context context;
   private TreeMaker maker;
-  private JavacElements elements;
+  private JavacElements elementUtils;
   private ParserFactory parserFactory;
+  private Names names;
 
   @Override
   public synchronized void init(ProcessingEnvironment processingEnv) {
-    trees = Trees.instance(processingEnv);
+    super.init(processingEnv);
+    this.elementUtils = ((JavacElements) processingEnv.getElementUtils());
     context = ((JavacProcessingEnvironment) processingEnv).getContext();
-    maker = TreeMaker.instance(context);
-    elements = JavacElements.instance(context);
-    parserFactory = ParserFactory.instance(context);
+    this.maker = TreeMaker.instance(context);
+    names = Names.instance(context);
   }
 
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-    roundEnv.getRootElements().stream().map(this::toUnit).forEach(compilationUnitTree -> {
-      System.out.println("----CompilationUnitTree----");
-      compilationUnitTree.accept(new AuthorVisitor(), null);
-      System.out.println(compilationUnitTree);
-    });
-    return false;
-  }
-
-  private CompilationUnitTree toUnit(Element element) {
-    TreePath path = trees.getPath(element);
-    return path.getCompilationUnit();
-  }
-
-  private class AuthorVisitor extends TreeScanner<Void, Void> {
-
-    @Override
-    public Void visitMethod(MethodTree node, Void unused) {
-      if (node instanceof JCMethodDecl) {
-        JCMethodDecl md = (JCMethodDecl) node;
-        if ("getAuthor".equals(md.name.toString())) {
-          md.body.stats =
-              List.of(parserFactory.newParser("return \"hello\";", false, false, false)
-                  .parseStatement());
-        }
+    Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Author.class);
+    System.out.println("elements.size() = " + elements.size());
+    if (!elements.isEmpty()) {
+      for (Element element : elements) {
+        JCClassDecl classDecl = (JCClassDecl) elementUtils.getTree(element);
+        String className = element.getSimpleName().toString();
+        System.out.println("className = " + className);
+        ListBuffer<JCTree.JCStatement> methodBlockStatements = new ListBuffer<>();
+        System.out.println("names = " + names);
+        JCReturn jcReturn = maker.Return(maker.Ident(names.fromString("String")));
+        JCBlock block = maker.Block(0, methodBlockStatements.toList());
+        methodBlockStatements.add(jcReturn);
+        classDecl.defs = classDecl.defs.prepend(
+            maker.MethodDef(maker.Modifiers(Flags.PUBLIC),
+                names.fromString("getAuthor"),
+                maker.Ident(names.fromString("String")),
+                List.nil(),
+                List.nil(),
+                List.nil(),
+                block,
+                null));
+        System.out.println("classDecl = " + classDecl);
       }
-      return super.visitMethod(node, unused);
     }
+    return false;
   }
 }
